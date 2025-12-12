1ab56a26cbe43d37673541fbdafe4c3f1029cb22
biscoo <eng.beshoy.91@outlook.com>
2025-12-12 14:01:30 +0100

    Create main.py

---
+++ Day-20-23/main.py
@@ -0,0 +1,44 @@
+from turtle import Turtle, Screen
+from snake import Snake
+from food import Food
+from score_board import Score
+import time
+
+screen = Screen()
+screen.screensize(600, 600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score = Score()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
+game_over = False
+
+while not game_over:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Food collision
+    if snake.head.distance(food) < 15:
+        food.new_food()
+        score.update_score()
+        snake.increase_snake_body()
+
+    # Wall collision
+    if snake.hit():
+        game_over = score.game_over()
+
+    # Body collision
+    if snake.body_collision():
+        game_over = score.game_over()
+
+screen.exitonclick()
